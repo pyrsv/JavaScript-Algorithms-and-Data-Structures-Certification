477e6a9b14c0cbcb01c9f7889f62b5a64f326f16
Nikita Pyresev <wegofender@gmail.com>
2019-12-01 19:19:20 +0200

    cash register fixed floating values

--- cash-register/script.js
+++ cash-register/script.js
@@ -1,13 +1,13 @@
 let VALUES = {
-    'ONE HUNDRED': 100,
-    'TWENTY': 20,
-    'TEN': 10,
-    'FIVE': 5,
-    'ONE': 1,
-    'QUARTER': 0.25,
-    'DIME': 0.1,
-    'NICKEL': 0.05,
-    'PENNY': 0.01,
+    'ONE HUNDRED': 10000,
+    'TWENTY': 2000,
+    'TEN': 1000,
+    'FIVE': 500,
+    'ONE': 100,
+    'QUARTER': 25,
+    'DIME': 10,
+    'NICKEL': 5,
+    'PENNY': 1,
 };
 
 function checkCashRegister(price, cash, cid) {
@@ -17,38 +17,28 @@ function checkCashRegister(price, cash, cid) {
     };
     let changeList = [];
 
-    let changeValue = cash - price;
+    let changeValue = (cash - price) * 100;
     let cidSum = cid
         .map(([, amount]) => amount)
-        .reduce((accum, amount) => (accum * 100 + amount * 100) / 100, 0);
-    // console.log('change val', changeValue);
+        .reduce((accum, amount) => (accum + amount * 100), 0);
 
-    cid.reverse().forEach(([unit, amount]) => {
-        // console.log(unit);
-        let count = 0;
-        let unitValue = VALUES[unit];
+    cid
+        .map(([unit, val]) => [unit, val * 100])
+        .reverse()
+        .forEach(([unit, amount]) => {
+            let count = 0;
+            let unitValue = VALUES[unit];
+            while (changeValue >= VALUES[unit] && amount > 0) {
+                changeValue -= unitValue;
+                cidSum -= unitValue;
+                amount -= unitValue;
+                count++
+            }
+            if (count > 0) {
+                changeList.push([unit, VALUES[unit] * count / 100])
+            }
+        });
 
-        // console.log(VALUES[unit]);
-        // if (changeValue >= VALUES[unit]) {
-        //     console.log(changeValue , unit, amount);
-        // }
-
-        while (changeValue >= VALUES[unit] && amount > 0) {
-            // console.log('a');
-            console.log('unitValue', unitValue);
-            changeValue -= unitValue;
-            cidSum -= unitValue;
-            amount -= unitValue;
-            console.log(amount);
-
-            count++
-        }
-        if (count > 0) {
-            changeList.push([unit, VALUES[unit] * count])
-        }
-    });
-
-    // console.log('change list:', changeList);
 
     if (cidSum < changeValue || changeValue !== 0) {
         answer.status = 'INSUFFICIENT_FUNDS'
@@ -56,13 +46,10 @@ function checkCashRegister(price, cash, cid) {
         answer.status = 'CLOSED';
         answer.change = changeList
     } else answer.change = changeList;
-    // if (changeValue < 0)
-
-
-    // console.log(changeValue);
-    // console.log(cidSum);
 
     return answer;
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]]));
+console.log(checkCashRegister(3.26, 100, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]]));
+console.log(checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]));
